0a705aa94e93d9d780bfb1721b7347e2f8ba071e
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2023-01-16 14:14:04 +0100

    Track concurrent requests (#1)
    * Track concurrent requests
    
    * Concurrent calls 'query' should just be the time series
    
    * Remove unused build.rs file
    
    * Group concurrent calls by function and module

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ pub use autometrics_macros::autometrics;
 // Not public API.
 #[doc(hidden)]
 pub mod __private {
-    use opentelemetry::{global, metrics::Histogram, Key, KeyValue, Value};
+    use opentelemetry::metrics::{Histogram, UpDownCounter};
+    use opentelemetry::{global, Key, KeyValue, Value};
 
     pub use crate::result_labels::*;
     pub use const_format::str_replace;
@@ -16,6 +17,35 @@ pub mod __private {
             .f64_histogram(name)
             .with_description("Autometrics histogram for tracking function calls")
             .init()
+    }
+
+    /// Increment the gauge by 1 and then decrement it again when the returned value is dropped
+    pub fn create_concurrency_tracker(name: &'static str, labels: [KeyValue; 2]) -> GaugeGuard {
+        let counter = global::meter("")
+            .i64_up_down_counter(name)
+            .with_description("Autometrics gauge for tracking concurrent function calls")
+            .init();
+        let context = Context::current();
+        counter.add(&context, 1, &labels);
+
+        GaugeGuard {
+            counter,
+            labels,
+            context,
+        }
+    }
+
+    /// Decrease the gauge by 1 when the guard is dropped
+    pub struct GaugeGuard {
+        counter: UpDownCounter<i64>,
+        labels: [KeyValue; 2],
+        context: Context,
+    }
+
+    impl Drop for GaugeGuard {
+        fn drop(&mut self) {
+            self.counter.add(&self.context, -1, &self.labels);
+        }
     }
 
     pub fn create_labels(function_name: &'static str, module: &'static str) -> [KeyValue; 2] {
