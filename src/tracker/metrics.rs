a9d3a9ae9061a9b30ef85839dd95431b2013ee43
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2023-01-30 18:48:03 +0100

    Add support for using metrics-rs crate

---
+++ src/tracker/metrics.rs
@@ -0,0 +1,55 @@
+use crate::{constants::*, labels::Label, tracker::TrackMetrics};
+use metrics::{
+    describe_counter, describe_gauge, describe_histogram, register_counter, register_gauge,
+    register_histogram, Gauge,
+};
+use std::{sync::Once, time::Instant};
+
+static ONCE: Once = Once::new();
+
+fn describe_metrics() {
+    ONCE.call_once(|| {
+        describe_counter!(COUNTER_NAME, COUNTER_DESCRIPTION);
+        describe_histogram!(HISTOGRAM_NAME, HISTOGRAM_DESCRIPTION);
+        describe_gauge!(GAUGE_NAME, GAUGE_DESCRIPTION);
+    });
+}
+
+pub struct MetricsTracker {
+    module: &'static str,
+    function: &'static str,
+    gauge: Gauge,
+    start: Instant,
+}
+
+impl TrackMetrics for MetricsTracker {
+    fn function(&self) -> &'static str {
+        self.function
+    }
+
+    fn module(&self) -> &'static str {
+        self.module
+    }
+
+    fn start(function: &'static str, module: &'static str) -> Self {
+        describe_metrics();
+
+        let gauge = register_gauge!(GAUGE_NAME, "function" => function, "module" => module);
+        gauge.increment(1.0);
+
+        Self {
+            module,
+            function,
+            gauge,
+            start: Instant::now(),
+        }
+    }
+
+    fn finish<'a>(self, counter_labels: &[Label]) {
+        let duration = self.start.elapsed().as_secs_f64();
+        register_counter!(COUNTER_NAME, counter_labels).increment(1);
+        register_histogram!(HISTOGRAM_NAME, "function" => self.function, "module" => self.module)
+            .record(duration);
+        self.gauge.decrement(1.0);
+    }
+}
--- src/tracker/mod.rs
+++ src/tracker/mod.rs
@@ -1,5 +1,7 @@
 use crate::labels::Label;
 
+#[cfg(feature = "metrics")]
+mod metrics;
 #[cfg(feature = "opentelemetry")]
 mod opentelemetry;
 #[cfg(feature = "prometheus")]
