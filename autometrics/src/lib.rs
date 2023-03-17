1a6f478862a7135f473085081fd49006c9ce1e7f
Roy Jacobs <roy.jacobs@gmail.com>
2023-03-17 10:31:14 +0100

    Update autometrics/src/lib.rs
    Co-authored-by: Evan Schwartz <3262610+emschwartz@users.noreply.github.com>

--- autometrics/src/lib.rs
+++ autometrics/src/lib.rs
@@ -15,7 +15,6 @@ mod tracker;
 
 pub use autometrics_macros::autometrics;
 pub use labels::GetLabel;
-pub use objectives::{Objective, ObjectivePercentage, TargetLatency};
 
 // Optional exports
 #[cfg(feature = "prometheus-exporter")]
