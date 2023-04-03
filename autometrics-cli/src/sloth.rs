5166ffe5babb95776c15f3ae99bd98f5566a8dd8
Gerry Agbobada <gerry@fiberplane.com>
2023-04-03 13:32:37 +0200

    Review
    - Reword new argument
    - Change unit

--- autometrics-cli/src/sloth.rs
+++ autometrics-cli/src/sloth.rs
@@ -10,19 +10,18 @@ pub struct Arguments {
     #[clap(long, default_values = &["90", "95", "99", "99.9"])]
     objectives: Vec<String>,
 
-    /// Minimum traffic to trigger alerts, specified as events/second.
+    /// Minimum traffic to trigger alerts, specified as events/minute.
     ///
-    /// If a service (all function sharing the same service name and objective
-    /// percentage) has less than this many calls per second, then it won't
-    /// trigger any alert.
+    /// Alerts will only trigger for an objective if the total call-rate of functions
+    /// comprising the objective is greather than this threshold.
     ///
-    /// Defaults to "at least 1 event per minute" (which is around
-    /// 0.16666...).
+    /// Defaults to "at least 1 event per minute"
     ///
-    /// Note that if a SLO has the same service name but with different
-    /// objective targets (as in "objective percentage") then the threshold
-    /// won't behave as expected.
-    #[clap(short, long, default_value_t = 0.16666666)]
+    /// Note that the total of calls is made on matching _both_ the "name"
+    /// attribute and the percentile targets; e.g. a function from an "API, 90%"
+    /// objective and one from an "API, 99%" objective count for 2 separate
+    /// low-traffic threshold.
+    #[clap(short, long, default_value_t = 1.0)]
     alerting_traffic_threshold: f64,
 
     /// Output path where the SLO file should be written.
@@ -34,7 +33,8 @@ pub struct Arguments {
 
 impl Arguments {
     pub fn run(&self) {
-        let sloth_file = generate_sloth_file(&self.objectives, self.alerting_traffic_threshold);
+        let sloth_file =
+            generate_sloth_file(&self.objectives, self.alerting_traffic_threshold / 60.0);
         if let Some(output_path) = &self.output {
             write(output_path, sloth_file)
                 .unwrap_or_else(|err| panic!("Error writing SLO file to {output_path:?}: {err}"));
@@ -76,7 +76,7 @@ fn generate_success_rate_slo(objective_percentile: &str, min_calls_per_second: f
     sli:
       events:
         error_query: sum by (objective_name, objective_percentile) (rate(function_calls_count{{objective_percentile=\"{objective_percentile}\",result=\"error\"}}[{{{{.window}}}}]))
-        total_query: sum by (objective_name, objective_percentile) (rate(function_calls_count{{objective_percentile=\"{objective_percentile}\"}}[{{{{.window}}}}])) > {min_calls_per_second}
+        total_query: sum by (objective_name, objective_percentile) (rate(function_calls_count{{objective_percentile=\"{objective_percentile}\"}}[{{{{.window}}}}])) >= {min_calls_per_second}
     alerting:
       name: High Error Rate SLO - {objective_percentile}%
       labels:
@@ -108,7 +108,7 @@ fn generate_latency_slo(objective_percentile: &str, min_calls_per_second: f64) -
             and
             label_join(rate(function_calls_duration_bucket{{objective_percentile=\"{objective_percentile}\"}}[{{{{.window}}}}]), \"autometrics_check_label_equality\", \"\", \"le\")
           ))
-        total_query: sum by (objective_name, objective_percentile) (rate(function_calls_duration_count{{objective_percentile=\"{objective_percentile}\"}}[{{{{.window}}}}])) > {min_calls_per_second}
+        total_query: sum by (objective_name, objective_percentile) (rate(function_calls_duration_count{{objective_percentile=\"{objective_percentile}\"}}[{{{{.window}}}}])) >= {min_calls_per_second}
     alerting:
       name: High Latency SLO - {objective_percentile}%
       labels:
