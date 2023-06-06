387ad7c1c26aa08a6836b73fa1a5dab8316dce18
egtw <emanuele.granato@nimisconsulting.com>
2023-06-06 19:47:51 +0200

    [WIP] OTLP push controller example initial commit

---
+++ examples/otlp-push-controller/src/main.rs
@@ -0,0 +1,51 @@
+use autometrics::autometrics;
+use opentelemetry::{runtime, Context};
+use opentelemetry::sdk::export::metrics::aggregation::cumulative_temporality_selector;
+use opentelemetry::sdk::metrics::controllers::BasicController;
+use opentelemetry::sdk::metrics::selectors;
+use opentelemetry::metrics;
+use opentelemetry_otlp::{ExportConfig, WithExportConfig};
+use tokio::time::sleep;
+use std::error::Error;
+use std::time::Duration;
+
+fn init_metrics() -> metrics::Result<BasicController> {
+    let export_config = ExportConfig {
+        endpoint: "http://localhost:4317".to_string(),
+        ..ExportConfig::default()
+    };
+    opentelemetry_otlp::new_pipeline()
+        .metrics(
+            selectors::simple::inexpensive(),
+            cumulative_temporality_selector(),
+            runtime::Tokio,
+        )
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_export_config(export_config),
+        )
+        .build()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
+    let meter_provider = init_metrics()?;
+    let cx = Context::current();
+
+    for numb in 0..10 {
+        do_stuff(numb).await;
+    }
+
+    println!("Waiting so that we could see metrics being pushed via OTLP every 10 seconds...");
+    sleep(Duration::from_secs(60)).await;
+    meter_provider.stop(&cx)?;
+
+    Ok(())
+}
+
+#[autometrics]
+async fn do_stuff(numb: u64) {
+    println!("Doing stuff for {} seconds...", numb);
+    sleep(Duration::from_secs(numb)).await;
+}
