2b43d0a1efdb8d86ba58857562c086ce4bca3528
Mari <me@cutegirl.tech>
2023-10-25 10:24:26 +0200

    add better error message when using `async-trait` (#149)
    * add better error message when using `async-trait`
    
    * change output file
    
    * move #121 compile tests into trybuild harness

---
+++ autometrics/tests/result_labels/fail/async_trait_support.rs
@@ -0,0 +1,34 @@
+use async_trait::async_trait;
+use autometrics::autometrics;
+
+// https://github.com/autometrics-dev/autometrics-rs/issues/141
+
+#[async_trait]
+trait TestTrait {
+    async fn method() -> bool;
+    async fn self_method(&self) -> bool;
+}
+
+#[derive(Default)]
+struct TestStruct;
+
+#[autometrics]
+#[async_trait]
+impl TestTrait for TestStruct {
+    async fn method() -> bool {
+        true
+    }
+
+    async fn self_method(&self) -> bool {
+        true
+    }
+}
+
+fn main() {
+    let ts = TestStruct::default();
+
+    async move {
+        <TestStruct as TestTrait>::method().await;
+        ts.self_method().await;
+    };
+}
