5e9262ee1dbcb1176c7bf72f8b339d47312117b1
Marvin Hansen <marvin.hansen@gmail.com>
2023-12-03 18:02:49 +0800

    Improved docs and code formatting.
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- examples/grpc-http/src/shutdown.rs
+++ examples/grpc-http/src/shutdown.rs
@@ -2,13 +2,13 @@ use crate::db_manager::DBManager;
 
 // Signal sender is non-clonable therefore we need to create a new one for each server.
 // https://github.com/rust-lang/futures-rs/issues/1971
-pub async fn http_sigint() {
+pub(crate) async fn http_sigint() {
     wait_for_signal().await;
 
     println!("http shutdown complete");
 }
 
-pub async fn grpc_sigint(dbm: DBManager) {
+pub(crate) async fn grpc_sigint(dbm: DBManager) {
     wait_for_signal().await;
 
     // Shutdown the DB connection.
@@ -19,8 +19,8 @@ pub async fn grpc_sigint(dbm: DBManager) {
 
     println!("gRPC shutdown complete");
 }
-/// Registers signal handlers and waits for a signal that
-/// indicates a shutdown request.
+
+/// Registers signal handlers and waits for a signal that indicates a shutdown request.
 pub(crate) async fn wait_for_signal() {
     wait_for_signal_impl().await
 }
@@ -30,13 +30,14 @@ pub(crate) async fn wait_for_signal() {
 async fn wait_for_signal_impl() {
     use tokio::signal::unix::{signal, SignalKind};
 
-    // https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+    // Docs: https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
     let mut signal_terminate = signal(SignalKind::terminate()).unwrap();
     let mut signal_interrupt = signal(SignalKind::interrupt()).unwrap();
     let mut signal_quit = signal(SignalKind::quit()).unwrap();
 
+    // https://docs.rs/tokio/latest/tokio/macro.select.html
     tokio::select! {
-        _ = signal_terminate.recv() => {println!("Received SIGTERM")},
+        _ = signal_terminate.recv() => println!("Received SIGTERM"),
         _ = signal_interrupt.recv() => println!("Received SIGINT"),
         _ = signal_quit.recv() => println!("Received SIGQUIT"),
     }
@@ -47,13 +48,13 @@ async fn wait_for_signal_impl() {
 async fn wait_for_signal_impl() {
     use tokio::signal::windows;
 
-    // Infos here:
-    // https://learn.microsoft.com/en-us/windows/console/handlerroutine
+    // Docs: https://learn.microsoft.com/en-us/windows/console/handlerroutine
     let mut signal_c = windows::ctrl_c().unwrap();
     let mut signal_break = windows::ctrl_break().unwrap();
     let mut signal_close = windows::ctrl_close().unwrap();
     let mut signal_shutdown = windows::ctrl_shutdown().unwrap();
 
+    // https://docs.rs/tokio/latest/tokio/macro.select.html
     tokio::select! {
         _ = signal_c.recv() => println!("Received CTRL_C."),
         _ = signal_break.recv() => println!("Received CTRL_BREAK."),
