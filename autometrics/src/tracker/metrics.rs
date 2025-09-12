7c5c1d1f73f961b27b65e43233b0d9a9d609d9af
Mari <git@cutegirl.tech>
2025-09-12 16:07:35 +0200

    fix metrics 0.24

--- autometrics/src/tracker/metrics.rs
+++ autometrics/src/tracker/metrics.rs
@@ -4,8 +4,7 @@ use crate::constants::*;
 use crate::labels::{BuildInfoLabels, CounterLabels, GaugeLabels, HistogramLabels};
 use crate::tracker::TrackMetrics;
 use metrics::{
-    describe_counter, describe_gauge, describe_histogram, register_counter, register_gauge,
-    register_histogram, Gauge, Unit,
+    counter, describe_counter, describe_gauge, describe_histogram, gauge, histogram, Gauge, Unit,
 };
 use std::{sync::Once, time::Instant};
 
@@ -35,7 +34,7 @@ impl TrackMetrics for MetricsTracker {
         describe_metrics();
 
         let gauge = if let Some(gauge_labels) = gauge_labels {
-            let gauge = register_gauge!(GAUGE_NAME, &gauge_labels.to_array());
+            let gauge = gauge!(GAUGE_NAME, &gauge_labels.to_array());
             gauge.increment(1.0);
             Some(gauge)
         } else {
@@ -50,8 +49,8 @@ impl TrackMetrics for MetricsTracker {
 
     fn finish(self, counter_labels: &CounterLabels, histogram_labels: &HistogramLabels) {
         let duration = self.start.elapsed().as_secs_f64();
-        register_counter!(COUNTER_NAME_PROMETHEUS, &counter_labels.to_vec()).increment(1);
-        register_histogram!(HISTOGRAM_NAME_PROMETHEUS, &histogram_labels.to_vec()).record(duration);
+        counter!(COUNTER_NAME_PROMETHEUS, &counter_labels.to_vec()).increment(1);
+        histogram!(HISTOGRAM_NAME_PROMETHEUS, &histogram_labels.to_vec()).record(duration);
         if let Some(gauge) = self.gauge {
             gauge.decrement(1.0);
         }
@@ -59,7 +58,7 @@ impl TrackMetrics for MetricsTracker {
 
     fn set_build_info(build_info_labels: &BuildInfoLabels) {
         SET_BUILD_INFO.call_once(|| {
-            register_gauge!(BUILD_INFO_NAME, &build_info_labels.to_vec()).set(1.0);
+            gauge!(BUILD_INFO_NAME, &build_info_labels.to_vec()).set(1.0);
         });
     }
 
@@ -67,7 +66,7 @@ impl TrackMetrics for MetricsTracker {
     fn intitialize_metrics(function_descriptions: &[FunctionDescription]) {
         for function in function_descriptions {
             let labels = &CounterLabels::from(function).to_vec();
-            register_counter!(COUNTER_NAME, labels).increment(0);
+            counter!(COUNTER_NAME, labels).increment(0);
         }
     }
 }
